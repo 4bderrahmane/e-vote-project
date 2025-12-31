7cc993ccf8eecfba6603e98852f1ae8af6e7dd6f
Abderrahmane <khbabez7@gmail.com>
2025-12-31 14:19:06 +0100

    adding KeycloakAdminGateway bean

--- e-vote-server/src/main/java/org/krino/voting_system/infrastructure/iam/config/KeycloakAdminConfiguration.java
+++ e-vote-server/src/main/java/org/krino/voting_system/infrastructure/iam/config/KeycloakAdminConfiguration.java
@@ -3,6 +3,8 @@ package org.krino.voting_system.infrastructure.iam.config;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
+import org.krino.voting_system.infrastructure.iam.KeycloakAdminClient;
+import org.krino.voting_system.infrastructure.iam.KeycloakAdminGateway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,5 +22,11 @@ public class KeycloakAdminConfiguration
                 .clientId(props.clientId())
                 .clientSecret(props.clientSecret())
                 .build();
+    }
+
+    @Bean
+    KeycloakAdminGateway keycloakAdminGateway(Keycloak keycloak, KeycloakAdminProperties props)
+    {
+        return new KeycloakAdminClient(keycloak, props);
     }
 }
