07210cf3bb8743be16d13f21f871e30e112d1abb
Abderrahmane <khbabez7@gmail.com>
2026-01-03 17:51:05 +0100

    removing a comment

--- hardhat/contracts/test/PoseidonT3Import.sol
+++ hardhat/contracts/test/PoseidonT3Import.sol
@@ -3,6 +3,4 @@ pragma solidity >=0.8.23 <0.9.0;
 
 import "poseidon-solidity/PoseidonT3.sol";
 
-// This file exists only to force Hardhat to compile PoseidonT3
-// so it can be deployed and linked in tests.
 contract PoseidonT3Import {}
