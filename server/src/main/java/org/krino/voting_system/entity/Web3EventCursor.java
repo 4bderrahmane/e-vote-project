fd02c6401582bee3bcc87271ca65d2b1848ffd15
Abderrahmane <khbabez7@gmail.com>
2026-01-03 23:46:34 +0100

    adding Web3EventCursor entity

---
+++ server/src/main/java/org/krino/voting_system/entity/Web3EventCursor.java
@@ -0,0 +1,27 @@
+package org.krino.voting_system.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigInteger;
+
+@Entity
+@Table(name = "web3_event_cursor")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Web3EventCursor
+{
+    @Id
+    @Column(name = "cursor_key", nullable = false, updatable = false, length = 128)
+    private String key;
+
+    // uint256-ish => BigInteger
+    @Column(name = "last_processed_block", nullable = false, precision = 78, scale = 0)
+    private BigInteger lastProcessedBlock;
+
+    @Column(name = "last_processed_log_index", nullable = false, precision = 78, scale = 0)
+    private BigInteger lastProcessedLogIndex;
+}
