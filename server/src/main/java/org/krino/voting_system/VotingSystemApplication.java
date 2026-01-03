3570f3fc46f74d211b560c0c9a2aa9e8acafab74
Abderrahmane <khbabez7@gmail.com>
2026-01-03 19:49:34 +0100

    removed unnecessary configuration annotation

--- server/src/main/java/org/krino/voting_system/VotingSystemApplication.java
+++ server/src/main/java/org/krino/voting_system/VotingSystemApplication.java
@@ -1,12 +1,9 @@
 package org.krino.voting_system;
 
-import org.krino.voting_system.infrastructure.iam.config.KeycloakAdminProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(KeycloakAdminProperties.class)
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class VotingSystemApplication
